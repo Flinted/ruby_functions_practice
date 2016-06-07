3ee0231223d50380e4873a185ea2e007e0fbab96
Chris Flint <flintedc@gmail.com>
2016-06-07 16:35:49 +0100

    tidied up cube volume calc

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -92,8 +92,8 @@ def number_to_short_month_name( number_month )
   end
 end
 
-def volume_of_cube( measure )
-  return measure ** 3
+def volume_of_cube( side )
+  return side ** 3
 end
 
 def volume_of_sphere( radius )
