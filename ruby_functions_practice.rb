a27ccc03101c72ada87d6ed62080e852c94cc48a
Chris Flint <flintedc@gmail.com>
2016-06-07 16:18:51 +0100

    added short month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -60,3 +60,34 @@ def number_to_full_month_name( number_month )
       return "Invalid Month"
   end
 end
+
+def number_to_short_month_name( number_month )
+  case number_month
+    when 1
+      return "Jan"
+    when 2
+      return "Feb"
+    when 3
+      return "Mar"
+    when 4
+      return "Apr"
+    when 5
+      return "May"
+    when 6
+      return "Jun"
+    when 7 
+      return "Jul"
+    when 8
+      return "Aug"
+    when 9
+      return "Sep"
+    when 10 
+      return "Oct"
+    when 11
+      return "Nov"
+    when 12 
+      return "Dec"
+    else
+      return "Invalid Month"
+  end
+end
